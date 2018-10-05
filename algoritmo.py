3d0bc4bc8a290c3c9cb50d94aa1ba1518075cf87
Justin <Justin.Vega.Madrigal@est.una.ac.cr>
2018-10-05 10:53:51 -0600

    Alpha del algoritmo de Markov
    Al menos parece que ya tengo una idea más clara.

--- algoritmo.py
+++ algoritmo.py
@@ -10,13 +10,37 @@ class Regla:
         self.etiqueta = etiqueta
         self.regla = regla
         self.longitud = len ( etiqueta )
-        self.auxiliar = []
-        for x in etiqueta:
-            self.auxiliar.append ( bool ( 0 ) )
+        
+    def funcionEspecial ( self, cadena ):
+        auxiliar = self.crearAuxiliar ( )
+        contador = 0
+        contadorDeEtiqueta = 0
+        for x in cadena:
+            if x == self.etiqueta [ contadorDeEtiqueta ]:
+                auxiliar [ contadorDeEtiqueta ] = bool ( 1 )
+                contadorDeEtiqueta = contadorDeEtiqueta + 1
+        contador = contador + 1
+        return self.cantidadDeMatches ( auxiliar )
+    
+    def crearAuxiliar ( self ):
+        auxiliar = [ ]
+        for x in self.etiqueta:
+            auxiliar.append ( bool ( 0 ) )
+        return auxiliar
+
+    def cantidadDeMatches ( self, lista ):
+        matches = 0
+        for x in lista:
+            if x == bool ( 1 ):
+                matches = matches = 1
+        if self.longitud == matches:
+            return bool ( 1 )
+        else:
+            return bool ( 0 )
 
 R1 = Regla ( 'A', 'apple' )
 
+print ( R1.funcionEspecial ( 'A' ) )
 print ( R1.etiqueta )
 print ( R1.regla )
 print ( R1.longitud )
-print ( R1.auxiliar )
