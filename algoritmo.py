b7afeef3c10e6c3fc8f5ba15b74c417c5d0e55bc
Justin <Justin.Vega.Madrigal@est.una.ac.cr>
2018-10-05 08:57:46 -0600

    Alpha del algoritmo de Markov
    Cambio de idea sobre como abordar el algoritmo de Markov.

--- algoritmo.py
+++ algoritmo.py
@@ -1,18 +1,22 @@
 #Autores: Justin Vega Madrigal (116550095)
 
-marcadores=['A', 'B', 'C', 'D', "the shop"]
-reglas = [ 'apple', 'bag', 'shop', "the", "my brother"]
-cadenaDeEntrada = [ 'D', 'C' ]
+marcadores=['A', 'B', 'C', 'D', "theshop"]
+reglas = [ 'apple', 'bag', 'shop', "the", "mybrother"]
+cadenaDeEntrada = 'DC'
 
-print ( marcadores )
-print ( reglas )
+class Regla:
+    
+    def __init__( self, etiqueta, regla ):
+        self.etiqueta = etiqueta
+        self.regla = regla
+        self.longitud = len ( etiqueta )
+        self.auxiliar = []
+        for x in etiqueta:
+            self.auxiliar.append ( bool ( 0 ) )
 
-simboloTerminal = bool ( 0 )
-auxiliar = 0
+R1 = Regla ( 'A', 'apple' )
 
-for a in cadenaDeEntrada:
-    auxiliar = 0
-    for b in marcadores:
-        auxiliar = auxiliar + 1
-        if a == b:
-            print ( b )
+print ( R1.etiqueta )
+print ( R1.regla )
+print ( R1.longitud )
+print ( R1.auxiliar )
