705467a45101e14aebcb0df9a5e264a615c70af6
EithanMM <30607390+EithanMM@users.noreply.github.com>
2018-10-06 20:52:55 -0700

    Cambios en interfaz: (#1)
    -Interfaz creada completamente, acomodada en forma de clases.
    -Funcionalidad agregada, ahora la aplicacion puede:
      -salvar gramaticas
      -editar gramaticas
      -abrir gramaticas
      -abrir una gramatica, editarla y salvarla bajo el mismo nombre
    
    -inclusion de alfabeto griego, en forma de paleta
    -los archivos estan validados para que acepte el alfabeto grieg
    -validacion para subir solo archivos .txt y .xml

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-# importando la ventana
-
+import tkinter as tk
+from tkinter import *
 from paradigmas import interfaz
 
-# accesando a iniciar la interfaz
-interfaz.window.mainloop()
+
+print (__name__)
+if __name__ == "__main__":
+ root = Tk()
+ root.geometry("600x600")
+ app = interfaz.ventana_principal(root)
+ root.mainloop()
